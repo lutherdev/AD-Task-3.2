932a75f7ac278f9a754283ab85f663b1560f059f
lutherdev <devturd@gmail.com>
2025-06-22 22:01:17 +0800

    refactor: fixed paths

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-define('BASE_PATH', realpath(__DIR__));
+if (!defined('BASE_PATH')) define('BASE_PATH', realpath(__DIR__));
+if (!defined('UTILS_PATH')) define('UTILS_PATH', BASE_PATH . '/utils');
+
 
 chdir(BASE_PATH);
 
