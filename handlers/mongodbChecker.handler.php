25775afd2781abfc3f5d82f86be622cc93ebb73a
lutherdev <devturd@gmail.com>
2025-06-22 20:12:05 +0800

    feat: added the global variable typeConfig to use env values

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,11 +1,16 @@
 <?php
+
+require_once __DIR__ . '/../utils/envSetter.util.php';
+
+$mongoUri = $typeConfig['mongo_uri'];
+
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:27123");
+    $mongo = new MongoDB\Driver\Manager($mongoUri);
 
     $command = new MongoDB\Driver\Command(["ping" => 1]);
     $mongo->executeCommand("admin", $command);
 
-    echo "✅ Connected to MongoDB successfully.  <br>";
+    echo "✅ Connected to MongoDB successfully. WEH  <br>";
 } catch (MongoDB\Driver\Exception\Exception $e) {
     echo "❌ MongoDB connection failed: " . $e->getMessage() . "  <br>";
 }
--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,12 +1,17 @@
 <?php
 
-$host = "host.docker.internal"; 
-$port = "5678";
-$username = "user";
-$password = "password";
-$dbname = "calendardb";
+require_once __DIR__ . '/../utils/envSetter.util.php';
 
-$conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
+// $host = $_ENV['PG_HOST'];
+// $port = $_ENV['PG_PORT'];
+// $username = $_ENV['PG_USER'];
+// $password = $_ENV['PG_PASS'];
+// $dbname = $_ENV['PG_DB'];
+
+
+//$conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
+$conn_string = "host={$typeConfig['pg_host']} port={$typeConfig['pg_port']} dbname={$typeConfig['pg_db']} user={$typeConfig['pg_user']} password={$typeConfig['pg_pass']}";
+
 
 $dbconn = pg_connect($conn_string);
 
@@ -14,6 +19,6 @@ if (!$dbconn) {
     echo "❌ Connection Failed: ", pg_last_error() . "  <br>";
     exit();
 } else {
-    echo "✔️ PostgreSQL Connection  <br>";
+    echo "✔️ PostgreSQL Connection <br>";
     pg_close($dbconn);
 }
