e2e21a8152459c8640f6e4222a685a3f9be6106a
lutherdev <devturd@gmail.com>
2025-06-24 15:51:13 +0800

    refactor: renamed file name

--- database/task.model.sql
+++ database/task.model.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tasks (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    project_id INT,
+    meetings_id INT,
     assigned_user_id INT,
     title VARCHAR(255),
     description TEXT,
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS tasks (
     due_date DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT fk_task_project FOREIGN KEY (project_id) REFERENCES projects(id),
+    CONSTRAINT fk_task_meetings FOREIGN KEY (meetings_id) REFERENCES meetings(id),
     CONSTRAINT fk_task_user FOREIGN KEY (assigned_user_id) REFERENCES users(id)
 );
