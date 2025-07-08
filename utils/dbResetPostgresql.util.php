7e6c039e1fd9d6ae3b267080792e1cbc29860f49
lutherdev <devturd@gmail.com>
2025-07-08 20:15:33 +0800

    fix: replaced variable name

--- utils/dbResetPostgresql.util.php
+++ utils/dbResetPostgresql.util.php
@@ -1,27 +1,11 @@
 <?php
 declare(strict_types=1);
-
 require_once 'bootstrap.php';
 
-$pgConfig = [
-'host' => $_ENV['PG_HOST'],
-'port' => $_ENV['PG_PORT'],
-'db'   => $_ENV['PG_DB'],
-'user' => $_ENV['PG_USER'],
-'pass' => $_ENV['PG_PASS'],
-];
+$dsn = "pgsql:host={$databases['pg_host']};port={$databases['pg_port']};dbname={$databases['pg_db']}";
 
-
-if (file_exists('/.dockerenv')) {
-    $pgConfig['host'] = $_ENV['PG_HOST'] = 'postgresql';
-    $pgConfig['port'] = '5432'; 
-} else {
-    $pgConfig['host'] = 'localhost';
-}
-
-$dsn = "pgsql:host={$pgConfig['host']};port={$pgConfig['port']};dbname={$pgConfig['db']}";
 try {
-$pdo = new PDO($dsn, $pgConfig['user'], $pgConfig['pass'], [
+$pdo = new PDO($dsn, $databases['pg_user'], $databases['pg_pass'], [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 ]);
 
@@ -30,18 +14,17 @@ echo "Connected to PostgreSQL!\n";
 $dbfiles = ['database/user.model.sql', 'database/meeting.model.sql', 'database/meeting_users.model.sql', 'database/task.model.sql'];
 
 foreach ($dbfiles as $dbfile){
-$num = 1;
 $sql = file_get_contents($dbfile);
 if (!$sql) {
-    throw new RuntimeException("❌ Could not read the SQL file");
+    throw new RuntimeException("❌ Could not read the SQL file" . $dbfile);
 }
-echo "✅ Tables $num created.\n";
+echo "✅ Tables $dbfile is created.\n";
 $pdo->exec($sql);
-$num++;
 }
 
 echo "✅ All Tables created successfully.\n";
 
+echo "Truncating tables…\n";
 foreach (['users', 'roles', 'groups'] as $table) {
     $pdo->exec("TRUNCATE TABLE {$table} RESTART IDENTITY CASCADE;");
     echo "the table $table has been truncated. \n";
