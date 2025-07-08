3c6ca7d0d14be0d0733d9f22f0af5e29e9f74f54
lutherdev <devturd@gmail.com>
2025-07-08 20:09:48 +0800

    feat: added a working dynamic landing page

--- index.php
+++ index.php
@@ -1,13 +1,26 @@
 <?php
-require_once 'bootstrap.php';
+session_start();
 
-?>
-<html>
-    <body>
-    <?php 
-    include_once HANDLERS_PATH . "/mongodbChecker.handler.php";
-    include_once HANDLERS_PATH . "/postgreChecker.handler.php";
-    include_once UTILS_PATH . "/dbResetPostgresql.util.php";
-    ?>
-    </body>
-</html>
+require_once BASE_PATH . '/bootstrap.php';
+require_once LAYOUTS_PATH . '/main.layout.php';
+require_once UTILS_PATH . '/auth.util.php';
+
+$uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), "/");
+if ($uri === "") {
+    $folder = "/homepage";
+} else {
+    $folder = $uri;
+}
+
+$pageFile = PAGES_PATH . "/{$folder}/index.php";
+$pageCssPath = "pages/{$folder}/assets/css/{$folder}.css";
+$title = ucfirst($folder);
+
+renderMainLayout(function () use ($pageFile) {
+    if (file_exists($pageFile)) {
+        require $pageFile;
+    } else {
+        echo "<h1>404 - Page Not Found</h1>";
+    }
+}, $title, $pageCssPath);
+
