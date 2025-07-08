b7283e27c1f78bc828300082b85bb44e6b49127d
lutherdev <devturd@gmail.com>
2025-07-08 20:11:26 +0800

    refactor: replaced role id and group id data type

--- database/user.model.sql
+++ database/user.model.sql
@@ -1,26 +1,14 @@
 CREATE TABLE IF NOT EXISTS groups(  
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS roles(  
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(255),
     level INT
 );
-
--- CREATE TABLE IF NOT EXISTS users(  
---     id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
---     username VARCHAR(255),
---     password VARCHAR(255),
---     full_name VARCHAR(255),
---     role_id uuid,             
---     group_id uuid, 
---     create_time DATE,
---     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
---     CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups(id)
--- );
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users(  
     id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -28,10 +16,11 @@ CREATE TABLE IF NOT EXISTS users(
     password VARCHAR(255),
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    role VARCHAR(255)            
+    role_id INTEGER REFERENCES roles(id) ON DELETE SET NULL,
+    group_id INTEGER REFERENCES groups(id) ON DELETE SET NULL           
 );
 
 COMMENT ON TABLE users IS 'Stores user account information';
 COMMENT ON COLUMN users.username IS 'Login username';
-COMMENT ON COLUMN users.role IS 'Foreign key to roles table';
--- COMMENT ON COLUMN users.group_id IS 'Foreign key to groups table';
+COMMENT ON COLUMN users.role_id IS 'Foreign key to roles table';
+COMMENT ON COLUMN users.group_id IS 'Foreign key to groups table';
