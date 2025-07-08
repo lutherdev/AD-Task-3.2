648da0774b54d52fc2c34ef676d635e2e7c843cd
lutherdev <devturd@gmail.com>
2025-07-08 20:06:53 +0800

    feat: added more paths

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,8 @@ define('TEMPLATES_PATH', realpath(BASE_PATH . '/components/templates'));
 define('STATICDATAS_PATH', realpath(BASE_PATH . '/staticDatas'));
 define('LAYOUTS_PATH', realpath(BASE_PATH . '/layouts'));
 define('ERRORS_PATH', realpath(BASE_PATH . '/errors'));
-
+define('PAGES_PATH', realpath(BASE_PATH . '/pages'));
+define('COMPONENTS_PATH', realpath(BASE_PATH . '/components'));
 chdir(BASE_PATH);
 
 require_once BASE_PATH . '/vendor/autoload.php';
